e4b329faf5622edc356419d8d948cab370c120f3
oran tal <otal@medallia.com>
2020-06-15 17:26:51 +0300

    change firebase database

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1fb93d3b0b87dd0165ad9eda7f48fc3.js"
+  "/precache-manifest.a40da7e81232e38440d003d5331ad53c.js"
 );
 
 self.addEventListener('message', (event) => {
